c336e0fb5a42998aadb2cecf7bcb411d1145edf1
Justin9091 <jga.raaijmakers@student.avans.nl>
2023-12-19 14:25:14 +0100

    Initial commit

---
+++ SpigotPluginTester/Plugin.cs
@@ -0,0 +1,11 @@
+﻿using SpigotFramework.Plugin;
+
+namespace SpigotPluginTester;
+
+public class Plugin : BasePlugin
+{
+    public override void OnEnable()
+    {
+        Console.WriteLine("PluginTester is up and running!");
+    }
+}
